b8822447068b4287a7b9c449f35b067df25a52cb
Ersin <ckmrsn@gmail.com>
2020-12-22 16:28:10 +0300

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text></Text>
+      <Text>Boş proje</Text>
       <StatusBar style="auto" />
     </View>
   );
