9f110a5292dd29c95983ba3ef2e1ab9cb728d1d1
ozgurnds <ozgurdes1999@gmail.com>
2020-12-26 01:08:46 +0300

    Tab Navigation
    Tab Navigation eklendi

--- pages/Mainpage.js
+++ pages/Mainpage.js
@@ -1,11 +1,77 @@
 import * as React from "react";
-import { Text, View, Button, Alert } from "react-native";
+import { Button, Text, View, Alert } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
 import * as firebase from "firebase";
 
-export default function App() {
+function DetailsScreen() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Maing Page!</Text>
+      <Text>Details!</Text>
+    </View>
+  );
+}
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+      />
+    </View>
+  );
+}
+
+function SettingsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Settings screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+      />
+    </View>
+  );
+}
+
+const HomeStack = createStackNavigator();
+
+function HomeStackScreen() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={HomeScreen} />
+      <HomeStack.Screen name="Details" component={DetailsScreen} />
+    </HomeStack.Navigator>
+  );
+}
+
+const SettingsStack = createStackNavigator();
+
+function SettingsStackScreen() {
+  return (
+    <SettingsStack.Navigator>
+      <SettingsStack.Screen name="Settings" component={SettingsScreen} />
+      <SettingsStack.Screen name="Details" component={DetailsScreen} />
+    </SettingsStack.Navigator>
+  );
+}
+
+const PostScreen = () => {
+  return (
+    <View>
+      <Text>Post Screen</Text>
+    </View>
+  );
+};
+
+const ProfileScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Profile Page!</Text>
       <Button
         title="Logout"
         onPress={() => {
@@ -23,5 +89,43 @@ export default function App() {
         }}
       />
     </View>
+  );
+};
+
+const Tab = createBottomTabNavigator();
+
+export default function Mainpage() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = focused ? "md-home" : "md-home-outline";
+            } else if (route.name === "Categories") {
+              iconName = focused ? "ios-list" : "ios-list-outline";
+            } else if (route.name === "Post") {
+              iconName = focused ? "add-circle" : "add-circle-outline";
+            } else if (route.name === "Profile") {
+              iconName = focused ? "ios-person" : "ios-person-outline";
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} color={color} size={25} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: "black",
+          inactiveTintColor: "gray",
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeStackScreen} />
+        <Tab.Screen name="Categories" component={SettingsStackScreen} />
+        <Tab.Screen name="Post" component={PostScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
